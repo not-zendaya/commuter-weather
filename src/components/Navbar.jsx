2ba3dbcd350f45f947134faf92990dc022f60a94
not-zendaya <ukzendaya@gmail.com>
2025-10-08 19:38:50 +0300

    applied some Tailwind styling to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () =>{
     return(
+        <div className="flex justify-around bg-blue-500 text-white p-3">
             <Link to="/" className ="hover:underline">Home </Link>
             <Link to="/forecast" className="hover:underline"> Forecast ⛅ </Link>
             <Link to="/search" className="hover:underline"> 🔍Search🔎</Link>
