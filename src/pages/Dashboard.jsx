2372de05beca82f04ceec505e7ef02f087e6a043
not-zendaya <ukzendaya@gmail.com>
2025-10-14 23:46:22 +0300

    feat: added dashboard screen with welcome and location setup

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Setup = () =>{
+    const [homeCity, setHomeCity] = useState("");
+    const [schoolCity, setSchoolCity] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
+    const handleSave = () =>{
+        if (!homeCity || !schoolCity){
+            setError ("Please enter both Home and School cities.");
+            return;
+        }
+
+        localStorage.setItem("homeCity", homeCity.trim());
+        localStorage.setItem("schoolCity", schoolCity.trim());
+
+        navigate("/");
+    };
+
+    return(
+        <div 
+                <div>
+                <h1 
+                <p 
+                    Get personalized weather updates for your daily routes!
+                    Let's Get Started 🚀
+                </p>
+                <button
+                    Set Locations
+                </button>
+                </div>
+        </div>
+    );
+};
+
+export default Setup;
