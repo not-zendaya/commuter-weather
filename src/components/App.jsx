93945582e67cb4e160ef3693ac2b308493f11121
not-zendaya <ukzendaya@gmail.com>
2025-10-16 17:16:06 +0300

    refactor: added Layout component for shared Navbar across all pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from '../pages/HomePage';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Layout from '../components/Layout';
 import Navbar from './Navbar';
+import Setup from '../pages/Dashboard';
+import Home from '../pages/HomePage';
 import Forecast from '../pages/Forecast';
+import About from '../pages/About';
+
 
 function App(){
   return(
     <Router>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-        <div className="flex-1 p-4">
+      <div className="h-screen bg-gray-50 text-gray-500">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/forecast" element={<Forecast />} />
-          <Route path="/search" element={<div>Search page coming soon....</div>} />
+          <Route element={<Layout />}>
+          <Route path="/" element={<Home /> } />
+          <Route path="/forecast" element={<Forecast /> } />
+          <Route path="/about" element={<About /> } />
+          <Route path="/setup" element={<Setup />} />
+          </Route>
         </Routes>
-        </div>
       </div>
     </Router>
   );
